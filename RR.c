08938ac7e8a43ffdfaee4267ccd58437534120dd
Ben <benjamin.gottheil@sjsu.edu>
2016-09-21 16:23:48 -0700

    basic implementation of round robin routine. Might not be ideall what the professor wants

--- RR.c
+++ RR.c
@@ -11,15 +11,18 @@ void RoundRobin(struct process* o_plist, float* runtime){ //original plist. Gonn
 	float servicetime;
 	memcpy(plist, o_plist, (sizeof(struct process) * NUM_PROCESS));
 	while(TRUE){
-		if(incomplete_process == 0) break;
-		*runtime += 1;
+		if(incomplete_process <= 0) break;
 		servicetime = plist[counter % NUM_PROCESS].service_time;
 		if(servicetime <= 1.0 && servicetime > 0.0){
+			*runtime += servicetime;
 			plist[counter % NUM_PROCESS].service_time = 0;
 			incomplete_process -= 1;
 		} 
-		else plist[counter % NUM_PROCESS].service_time -= 1.0;
-
+		else{
+			*runtime += 1;
+			plist[counter % NUM_PROCESS].service_time -= 1.0;
+		} 
+		counter++;
 	}
 	free(plist);
 	return;
