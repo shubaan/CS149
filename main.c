211ff238951cc77ff21efa37de42b6e49ed24278
nopenoway0 <kite670@yahoo.com>
2016-09-21 03:44:47 -0700

    added call to FCFS

--- main.c
+++ main.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "process_producer.h"
-
+#include "FCFS.h"
 
 int main(){
 	int seed = time(NULL);
+	float runtime;
 	srand(seed);
 	struct process* list;
 	CreateProcesses(&list);
 	PrintProcessList(list);
+	
+	runtime = FCFS(list);
+	printf("First Come First Server: %.2f\n", runtime);
 }
