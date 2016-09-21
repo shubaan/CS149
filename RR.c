691d9db340feae7892f57d35601f630fb8c0675a
Ben <benjamin.gottheil@sjsu.edu>
2016-09-21 16:13:50 -0700

    Start roundrobin code. Not properly written

---
+++ RR.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "process_producer.h"
+#define TRUE 1 //add header to store define variables
+
+void RoundRobin(struct process* o_plist, float* runtime){ //original plist. Gonna use copy so the arrays can be modified in the funciton
+	int counter = 0;
+	int incomplete_process = NUM_PROCESS;
+//ADD TEST IF MAX_QUANTA LESS THAN NUM_PROCESS
+	struct process* plist = malloc(sizeof(struct process) * NUM_PROCESS);
+	float servicetime;
+	memcpy(plist, o_plist, (sizeof(struct process) * NUM_PROCESS));
+	while(TRUE){
+		if(incomplete_process == 0) break;
+		*runtime += 1;
+		servicetime = plist[counter % NUM_PROCESS].service_time;
+		if(servicetime <= 1.0 && servicetime > 0.0){
+			plist[counter % NUM_PROCESS].service_time = 0;
+			incomplete_process -= 1;
+		} 
+		else plist[counter % NUM_PROCESS].service_time -= 1.0;
+
+	}
+	free(plist);
+	return;
+}
---
+++ RR.h
@@ -0,0 +1,8 @@
+#ifndef RR
+#define RR
+
+#include "process_producer.h"
+
+void RoundRobin(struct process* plist, float* runtime);
+
+#endif
