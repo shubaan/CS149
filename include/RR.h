a2c59b29892fb9a732b7bd7babc00c1977e9cc51
nopenoway0 <kite670@yahoo.com>
2016-09-22 01:20:37 -0700

    organization

--- include/RR.h
+++ include/RR.h
@@ -3,6 +3,9 @@
 
 #include "process_producer.h"
 
+/* Function: RoundRobin
+*  calculates the run time for an array of processes if each process got exactly 1 quanta time slice. The value is stored in the runtime variable
+*/
 void RoundRobin(struct process* plist, float* runtime);
 
 #endif
--- include/process_producer.h
+++ include/process_producer.h
@@ -4,6 +4,9 @@
 #define NUM_PROCESS 15
 #define MAX_QUANTA 100
 
+/* Struct: process
+*  struct created in accordance to Ezzat's specifications
+*/
 struct process{
 	int arrival_time;
 	float service_time;
@@ -11,8 +14,16 @@ struct process{
 	char name;
 };
 
+/* Function: PrintProcessList
+*--------------------------------
+* Pass in an array of struct process and it will print all attributes and processes
+*/
 void PrintProcessList(struct process* a);
 
+/* Function: CreateProcess
+* pass in a pointer, this function constructs an array of struct process. How many
+* process is determined by the macro NUM_PROCESS and the max arrival time is determined by MAX_QUANTA
+*/
 void CreateProcesses(struct process** process_list);
 
 #endif
