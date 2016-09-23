53a5450aacce1dd64a3b27ca7304f14a817ea3b4
Ben <benjamin.gottheil@sjsu.edu>
2016-09-22 18:11:20 -0700

    added todo because current algorithm doesn't take into account arrival time

--- include/FCFS.h
+++ include/FCFS.h
@@ -3,7 +3,7 @@
 
 /* Function: FirstCome
 *  takes in pointer to a process list with a number of elements defined by NUM_RPOCESS. runetime is a pointer to a float which will store the total runtime.
-*  
+*  TODO//currently doesn't work because it doesn't take into account arrival time
 */
 void FirstCome(struct process* plist, float* runtime);
 
