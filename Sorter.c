e4ef27bd127a3e22730b6c0ead231a3d5e5fd3df
nopenoway0 <kite670@yahoo.com>
2016-09-23 02:15:51 -0700

    basic function started. swap working

--- Sorter.c
+++ Sorter.c
@@ -4,5 +4,23 @@
 #include "Sorter.h"
 
 void SortBy(struct process** plist, int num){
+	if(num == 0) swap(&(*plist[0]), &(*plist)[1], sizeof(*plist[0]), sizeof(*plist[1]));
+	return;
+}
+
+void swap(void* objA, void* objB, size_t size_objA, size_t size_objB){
+
+	if(size_objA == size_objB){
+		void* tmp = malloc(size_objB);
+
+		memcpy(tmp, objA, size_objA);
+		memmove(objA, objB, size_objA);
+		memmove(objB, tmp, size_objA);
+
+		struct process* test = objB;
+		//printf("%c %f %d size: %d", (*test).name, (*test).service_time, (*test).priority, size_objB);
+		//printf("\nEntered\n");
+		free(tmp);
+	}
 	return;
 }
--- include/Sorter.h
+++ include/Sorter.h
@@ -5,8 +5,10 @@
 
 /* Function: SortBy
 *  sorts according to parameter, modifies the array directly
-* parameters are the list of processes, according to Num_PROCESS. The input num decides what field to sort by 0 - arrival time 1 - service time 2 - priority
+* parameters are the list of processes, according to Num_PROCESS. The input num decides what field to sort by 0 - arrival time; 1 - service time; 2 - priority
 */
 void SortBy(struct process** plist, int num);
+
+void swap(void* objA, void* objB, size_t size_objA, size_t size_objB);
 
 #endif
