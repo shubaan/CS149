c3720026f7e8c76050e7afb5fc3c5bceac496d26
Ben <benjamin.gottheil@sjsu.edu>
2016-09-24 21:59:47 -0700

    added test functions

--- Sorter.c
+++ Sorter.c
@@ -22,13 +22,41 @@ void SortBy(struct process** plist, int num){
 		head += inc_dec;
 	}
 	//PIVOT LOCATION - 1 WHAT IF 0?
-	SortByRec(&*plist, num, 0, pivot_location);
-	SortByRec(&*plist, num, pivot_location, NUM_PROCESS - 1);
+
+	// delete this print
+	PrintServiceTimes(&*plist);
+
+	//SortByRec(&*plist, num, 0, pivot_location);
+	//SortByRec(&*plist, num, pivot_location, NUM_PROCESS - 1);
 	return;
 }
 
 void SortByRec(struct process** plist, int num, int range_lo, int range_hi){
+	if((range_hi - range_lo) == 0) return;
+	int head = range_lo;
+	int inc_dec = 1;
+	int pivot_location = range_hi;
+	float service_time = (*plist)[pivot_location].service_time;
+	while(TRUE){
+		if(head == pivot_location) break;
+		if((*plist)[head].service_time > service_time){
+			swap(&(*plist)[head], &(*plist)[pivot_location], sizeof((*plist)[head]), sizeof((*plist)[pivot_location]));
+			swap(&pivot_location, &head, sizeof(pivot_location), sizeof(head));
+			inc_dec *= -1;
+			head += inc_dec;
+		}
+	}
 	return;
+}
+
+
+//REMOVE THIS FUNCTION
+void PrintServiceTimes(struct process** plist){
+	printf("\n( ");
+	for(int x = 0; x < NUM_PROCESS; x++){
+		printf("%.2f, ", (*plist)[x].service_time);
+	}
+	printf(" )\n");
 }
 
 void swap(void* objA, void* objB, size_t size_objA, size_t size_objB){
