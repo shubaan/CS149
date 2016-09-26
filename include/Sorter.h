b17f3dacba208578af30245659c72b677eb334e1
nopenoway0 <kite670@yahoo.com>
2016-09-26 00:52:54 -0700

    updated comments

--- include/Sorter.h
+++ include/Sorter.h
@@ -5,7 +5,7 @@
 
 /* Function: SortBy
 *  sorts according to parameter, modifies the array directly
-* parameters are the list of processes, according to Num_PROCESS. The input num decides what field to sort by 0 - arrival time; 1 - service time; 2 - priority
+* parameters are the list of processes, according to Num_PROCESS. The input num decides what field to sort by 0 - arrival time; 1 - service time; 2 - priority; 3 - actual start time; 4 - end time
 */
 void SortBy(struct process** plist, int num);
 
