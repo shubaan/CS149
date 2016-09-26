b33ebd442f4d94d2bdd6d8f3b4dcab5bdf3cf976
nopenoway0 <kite670@yahoo.com>
2016-09-26 00:51:47 -0700

    no big changes

--- include/FCFS.h
+++ include/FCFS.h
@@ -4,9 +4,14 @@
 /* Function: FirstCome
 *  takes in pointer to a process list with a number of elements defined by NUM_RPOCESS. runetime is a pointer to a float which will store the total runtime.
 *  TODO//currently doesn't work because it doesn't take into account arrival time
+*  DEPRECATED
 */
 void FirstCome(struct process* plist, float* runtime);
-char* getFCFSOrder(struct process** plist, int *size);
+
+/* Function: getFCFSOrder
+*  currently used first serve first come
+*/
+char* getFCFSOrder(struct process** plist, int size);
 
 #endif
 
