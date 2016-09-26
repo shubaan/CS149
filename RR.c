5c7a02e4867630e5b36c341e6ab68f9f9793a6ec
Ben <benjamin.gottheil@sjsu.edu>
2016-09-26 15:01:24 -0700

    continuing progress on RR. not printing or operating correctly

--- RR.c
+++ RR.c
@@ -8,6 +8,8 @@
 void RoundRobin(struct process* o_plist){ //original plist. Gonna use copy so the arrays can be modified in the funciton
 	int counter_char_array = 0;
 	int counter_process = 0;
+	int queue[NUM_PROCESS] = {0};
+	int available_service = 0;
 	int quanta;
 //ADD TEST IF MAX_QUANTA LESS THAN NUM_PROCESS
 	struct process* plist = malloc(sizeof(struct process) * NUM_PROCESS);
@@ -22,24 +24,33 @@ void RoundRobin(struct process* o_plist){ //original plist. Gonna use copy so th
 		counter_char_array++;
 	}
 
-	// NEED TO ACCOUNT FOR IDLE CPU
+	// NEED TO ACCOUNT FOR IDLE CPU - check for all 0's in array
 	while(TRUE){ 
 		if(quanta >= MAX_QUANTA) break;
-		else if(plist[counter_process % NUM_PROCESS].service_time == 0 || plist[counter_process % NUM_PROCESS].arrival_time > quanta) counter_process++; // cheap way to pretend process isn't in queue anymore, just skip over it, no time taken
-		else if(plist[counter_process % NUM_PROCESS].service_time < 1){ // because service time is a float, we need a special case to reduce it to exactly 0
-			plist[counter_process % NUM_PROCESS].service_time = 0;
+		for(int x = 0; x < NUM_PROCESS; x++){
+			if(plist[x].service_time == 0) queue[x] = 0;
+			else if(plist[x].service_time > 0 && plist[x].arrival_time <= quanta){
+				queue[x] = 1;
+				available_service = 1;
+			}
+			else queue[x] = 0;
+		}
+
+		if(available_service == 0){
+			quanta += 1;
+			available_service = 0;
+			rr_process_order[counter_char_array] = ' ';
+		}
+		else if(queue[counter_process % NUM_PROCESS] == 1){
+			printf("entered counter\n");
+			if(plist[counter_process % NUM_PROCESS].service_time < 1) plist[counter_process % NUM_PROCESS].service_time = 0;
+			else plist[counter_process % NUM_PROCESS].service_time -= 1;
 			rr_process_order[counter_char_array] = plist[counter_process % NUM_PROCESS].name;
 			counter_process++;
-			counter_char_array++;
-			quanta++;
+			quanta += 1;
 		}
-		else {
-			plist[counter_process % NUM_PROCESS].service_time - 1;
-			rr_process_order[counter_char_array] = plist[counter_process % NUM_PROCESS].name;
-			counter_char_array++; // always 1 timeslice per process
-			counter_process++;
-			quanta++;
-		}
+		else counter_process++;
+		counter_char_array++;
 	}
 
 	for(int x = 0; x < MAX_QUANTA; x++){
@@ -49,7 +60,7 @@ void RoundRobin(struct process* o_plist){ //original plist. Gonna use copy so th
 	printf("\n");
 
 	for(int x = 0; x < CHAR_ARRAYMAX; x++){
-		printf("%c ", plist[x].name);
+		printf("%c ", rr_process_order[x]);
 	}
 
 	free(plist);
