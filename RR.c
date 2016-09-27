ee33372d0fecb18fe37737a3c80000aa8abb5397
nopenoway0 <kite670@yahoo.com>
2016-09-27 14:12:27 -0700

    finished algorithm, need to bug fix

--- RR.c
+++ RR.c
@@ -11,6 +11,7 @@ void RoundRobin(struct process* o_plist){ //original plist. Gonna use copy so th
 	int queue[NUM_PROCESS] = {0};
 	int available_service = 0;
 	int quanta;
+	int head = 0; //start at beginning
 	char* rr_process_order = malloc(sizeof(char) * CHAR_ARRAYMAX);
 //ADD TEST IF MAX_QUANTA LESS THAN NUM_PROCESS
 	struct process* plist = malloc(sizeof(struct process) * NUM_PROCESS);
@@ -27,40 +28,40 @@ void RoundRobin(struct process* o_plist){ //original plist. Gonna use copy so th
 		counter_char_array++;
 	}
 
-	// NEED TO ACCOUNT FOR IDLE CPU - check for all 0's in array
 	while(TRUE){ 
 		if(quanta >= MAX_QUANTA) break;
-
-		//Checks how many processes are currently in queued, if there is a 1 in the array slot, there is process that has both arrived and has positive service time
+		//Checks how many processes are currently in queued, if there is a 1 in the array slot, there is process that has both arrived and has positive service time - can change to do so it always runs once
 		for(int x = 0; x < NUM_PROCESS; x++){
-			if(plist[x].service_time == 0) queue[x] = 0;
-			else if(plist[x].service_time > 0 && plist[x].arrival_time <= quanta){
-				queue[x] = 1;
+			if(plist[(x + head) % NUM_PROCESS].service_time == 0) queue[x] = 0;
+			else if(plist[(x + head) % NUM_PROCESS].service_time > 0 && plist[(x + head) % NUM_PROCESS].arrival_time <= quanta){
+				queue[(x + head) % NUM_PROCESS] = 1;
 				available_service = 1;
+				head = (x + head) % NUM_PROCESS;
+				break;
 			}
 			else queue[x] = 0;
 		}
 
 		// just for testing purposes
 		//for(int x = 0; x < NUM_PROCESS; x++){
-			//printf("%d  ", queue[x]);
+		//	printf("%d  ", queue[x]);
 		//}
-		
+		//printf("\n");
+		//
+
 		if(available_service == 0){
-			quanta += 1;
 			//available_service = 0; not needed
 			rr_process_order[counter_char_array] = ' ';
 			counter_char_array++;
 		}
-		else if(queue[counter_process % NUM_PROCESS] == 1){
-			if(plist[counter_process % NUM_PROCESS].service_time < 1) plist[counter_process % NUM_PROCESS].service_time = 0;
-			else plist[counter_process % NUM_PROCESS].service_time -= 1;
-			rr_process_order[counter_char_array] = plist[counter_process % NUM_PROCESS].name;
-			counter_process++;
+		else if(queue[head] == 1){
+			if(plist[head].service_time < 1) plist[head].service_time = 0;
+			else plist[head].service_time -= 1;
+			rr_process_order[counter_char_array] = plist[head].name;
 			counter_char_array++;
-			quanta += 1;
 		}
-		else counter_process++;
+		quanta++;
+		head = (head + 1) % NUM_PROCESS;
 	}
 
 	// print all quanta slices
