15461c0367655540308b7fcb12078ffd4c594174
nopenoway0 <kite670@yahoo.com>
2016-09-29 14:09:30 -0700

    Hfpa (#10)
    * Add files via upload
    
    * Add files via upload

--- main.c
+++ main.c
@@ -6,6 +6,8 @@
 #include "Sorter.h"
 #include "Calculation.h"
 #include <time.h>
+#include "HFPA.h"
+#include "PreHFPA.h"
 //TODO: CONGREGATE DEFINE DEFINTIONS IN OWN HEADERFILE
 
 void printProcessTracker();
@@ -17,11 +19,25 @@ int main(){
 	srand(seed);
 	struct process* list;
 	struct process* copy_list = malloc(sizeof(struct process) * NUM_PROCESS);
+	struct process* HPFA1_list = malloc(sizeof(struct process) * NUM_PROCESS);
+	struct process* HPFA2_list = malloc(sizeof(struct process) * NUM_PROCESS);
+
 	CreateProcesses(&list);
 	PrintProcessList(list);
 	memcpy(copy_list, list, sizeof(struct process) * NUM_PROCESS);
+	memcpy(HPFA1_list, list, sizeof(struct process) * NUM_PROCESS);
+	memcpy(HPFA2_list, list, sizeof(struct process) * NUM_PROCESS);
 	SortBy(&copy_list, 0); //The input num decides what field to sort by 0 - arrival time; 1 - service time; 2 - priority; 3 - actual start; 4 - end time
-	
+    SortBy(&HPFA1_list);
+    SortBY(&HPFA2_list);
+
+    printf("\n\n***********NONPREHPFA****************\n\n");
+	printProcessTracker();
+	printFCFSOrder(HPFA1_list);
+
+	printf("\n\n***********PREHPFA****************\n\n");
+	PreHFPA(HFPA2_list);
+
 	printf("\n\n***********FCFS****************\n\n");
 	printProcessTracker();
 	printFCFSOrder(copy_list);
@@ -55,7 +71,7 @@ void printFCFSOrder(struct process* plist) {
 	// Adjust to add extra spaces to match printProcessTracker();
 	// Char array doesn't have extra spaces to accommodate the difference
 	for (int i = 0; i < CHAR_ARRAYMAX; i++) {
-		if (charArray[i] == ' ' && i < 10) { 
+		if (charArray[i] == ' ' && i < 10) {
 			printf("  ");
 		} else if (charArray[i] == ' ' && i >= 10) {
 			printf("   ");
@@ -66,7 +82,7 @@ void printFCFSOrder(struct process* plist) {
 		}
 	}
 	printf("\n");
-	// Size is now the # of processes that completed its process 
+	// Size is now the # of processes that completed its process
 	printf("Average response time: %.2f\n", calAverageResponse(plist, process_ran));
 	printf("Average waiting time: %.2f\n", calAverageWaiting(plist, process_ran));
 	printf("Average turnaround time: %.2f\n", calAverageTurnaround(plist, process_ran));
