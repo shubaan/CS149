6a321576de57de33cd003e10e161bec340a44c5a
nopenoway0 <kite670@yahoo.com>
2016-09-28 23:56:22 -0700

    updated to remove deprecated function

--- include/FCFS.h
+++ include/FCFS.h
@@ -2,13 +2,6 @@
 #define FCFS
 #include "process_producer.h"
 
-/* Function: FirstCome
-*  takes in pointer to a process list with a number of elements defined by NUM_RPOCESS. runetime is a pointer to a float which will store the total runtime.
-*  TODO//currently doesn't work because it doesn't take into account arrival time
-*  DEPRECATED
-*/
-void FirstCome(struct process* plist, float* runtime);
-
 /* Function: getFCFSOrder
 *  currently used first serve first come
 */
