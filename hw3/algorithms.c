7b7c525bec3b9a4cd1f84cc0ca77e008642fe4d4
nopenoway0 <kite670@yahoo.com>
2016-10-03 15:30:50 -0700

    finished general use sorting. uses quick sort for any struct or object that has an integer to compare. Sorts from least to greatest

--- hw3/algorithms.c
+++ hw3/algorithms.c
@@ -11,22 +11,64 @@ static void swap(void* obj_a, void* obj_b, int size_a, int size_b){
 	free(tmp);
 }
 
-void sort(void* start_obj, int* int_comp, int obj_size, int length){
+static void sort_rec(void* start_obj, int* int_comp, int obj_size, int length){
 	//Set pointer to beginning of array
+	if(length == 0) return;
 	length -= 1;
-	int inc = 1; int pivot = length; int lo = 0;
-	char* lo; char* hi;
+	int inc = 1;
+	int pivot = length;
+	int p_loc = 0;
 	char* beg_objb = (char*) start_obj;
 	char* int_compb = (char*) int_comp;
-	pivot = (char*) start_obj;
-	pivot += (obj_size * length); 
 
 	//printf("Comp1: %d Comp2: %d\n", *( (int*) (int_compb) ), *( (int*) (int_compb + (obj_size * length) ) ) );
 	//printf("%d", *((int*)(beg_objb + (obj_size * length))))
-	for(int x = 0; x =< length; x++){
-		if( *( (int*) (int_compb) ) < *( (int*) (int_compb + (obj_size * x) ) ) ){
-			swap(beg_objb, (beg_objb + (obj_size * x)), obj_size, obj_size);
+	for(int x = 0; x <= length; x++){
+		if(pivot == p_loc) break;
+		if( *( (int*) (int_compb + (obj_size * pivot)) ) < *( (int*) (int_compb + (obj_size * p_loc) ) ) && pivot  > p_loc){
+			swap((beg_objb + (obj_size * pivot)), (beg_objb + (obj_size * p_loc)), obj_size, obj_size);
+			swap(&pivot, &p_loc, sizeof(int), sizeof(int));
+			inc *= -1;
 		}
+		else if( *( (int*) (int_compb + (obj_size * pivot)) ) > *( (int*) (int_compb + (obj_size * p_loc) ) ) && pivot < p_loc){
+			swap((beg_objb + (obj_size * pivot)), (beg_objb + (obj_size * p_loc)), obj_size, obj_size);
+			swap(&pivot, &p_loc, sizeof(int), sizeof(int));
+			inc *= -1;
+		}
+		p_loc += inc;
 	}
+	sort_rec((beg_objb + (obj_size * pivot)), (int*) (int_compb + (obj_size * pivot)), obj_size, length - pivot);
+	sort_rec(beg_objb, (int*) int_compb, obj_size, pivot);
+	return;
+}
+
+void sort(void* start_obj, int* int_comp, int obj_size, int length){
+	//Set pointer to beginning of array
+	length -= 1;
+	int inc = 1;
+	int pivot = length;
+	int p_loc = 0;
+	char* beg_objb = (char*) start_obj;
+	char* int_compb = (char*) int_comp;
+
+	//printf("Comp1: %d Comp2: %d\n", *( (int*) (int_compb) ), *( (int*) (int_compb + (obj_size * length) ) ) );
+	//printf("%d", *((int*)(beg_objb + (obj_size * length))))
+	for(int x = 0; x <= length; x++){
+		if(pivot == p_loc) break;
+		if( *( (int*) (int_compb + (obj_size * pivot)) ) < *( (int*) (int_compb + (obj_size * p_loc) ) ) && pivot  > p_loc){
+			swap((beg_objb + (obj_size * pivot)), (beg_objb + (obj_size * p_loc)), obj_size, obj_size);
+			swap(&pivot, &p_loc, sizeof(int), sizeof(int));
+			inc *= -1;
+		}
+		else if( *( (int*) (int_compb + (obj_size * pivot)) ) > *( (int*) (int_compb + (obj_size * p_loc) ) ) && pivot < p_loc){
+			swap((beg_objb + (obj_size * pivot)), (beg_objb + (obj_size * p_loc)), obj_size, obj_size);
+			swap(&pivot, &p_loc, sizeof(int), sizeof(int));
+			inc *= -1;
+		}
+		p_loc += inc;
+	}
+	//start at pivot
+	sort_rec((beg_objb + (obj_size * pivot)), (int*) (int_compb + (obj_size * pivot)), obj_size, length - pivot);
+	sort_rec(beg_objb, (int*) int_compb, obj_size, pivot);
 	return;
 }
--- hw3/include/algorithms.h
+++ hw3/include/algorithms.h
@@ -4,7 +4,7 @@
  * [sort sorts objects from smallest to largest]
  * @param start_obj [beginning of the array containing the list of objects]
  * @param int_comp  [pointer to the first integer contained in the object. this is the integer to compare]
- * @param obj_size  [size of each object in the array]
+ * @param obj_size  [size of each object/struct in the array]
  * @param length    [total amount of objects in the array]
  */
 void sort(void* start_obj, int* int_comp, int obj_size, int length);
@@ -17,4 +17,14 @@ void sort(void* start_obj, int* int_comp, int obj_size, int length);
  * @param size_b [size of object b - MUST BE SAME SIZE AS OBJECT A]
  */
 static void swap(void* obj_a, void* obj_b, int size_a, int size_b);
+
+/**
+ * [sort_rec recursive call from sort function. Uses quick sort to sort objects. It is static so it can only be called from algorithms.c]
+ * @param start_obj [beginning of the array containing the list of objects]
+ * @param int_comp  [pointer to the first integer contained in the object. this is the integer to compare]
+ * @param obj_size  [size of each object/struct in the array]
+ * @param length    [total amount of objects in the array]
+ */
+static void sort_rec(void* start_obj, int* int_comp, int obj_size, int length);
+
 #endif
